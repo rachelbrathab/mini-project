3c4d33857ee656fb00e39ce220836f993c0370d5
Rachel Brathab <rachelbrathab@SSRs-MacBook-Pro.local>
2026-04-15 02:10:33 +0530

    Fix import to use smartStudyPlanner.planner for deployment

--- smartStudyPlanner/app.py
+++ smartStudyPlanner/app.py
@@ -4,7 +4,7 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from flask import Flask, request, render_template, redirect, url_for, session, send_file, send_from_directory
 from flask import jsonify
-from planner import generate_schedule, format_time
+from smartStudyPlanner.planner import generate_schedule, format_time
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import json
